b9ba6231065728b5f53976f5b6040259a3a7f49c
Chris McGrath <chris@octopod.info>
2014-02-08 17:58:49 +0000

    limit/order search and format result message

--- spec/models/quote_request_spec.rb
+++ spec/models/quote_request_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
+describe QuoteRequest, ".perform" do
+  subject { QuoteRequest.new(location: location_name, product: product_name) }
+  let(:location_name) { "Bo" }
+  let(:product_name) { "seat" }
+
+  it "returns not found message if can't find products" do
+    subject.stub(:search).and_return([])
+    expect(subject.perform).to eql("Sorry, nothing found for #{product_name} near #{location_name}")
+  end
+
+  it "returns the inventories when they are found" do
+    inventory1 = Fabricate.build(:inventory, price: 1.00)
+    inventory2 = Fabricate.build(:inventory, price: 2.00)
+    subject.stub(:search).and_return([inventory1, inventory2])
+    expect(subject.perform).to include("#{inventory1.supplier.email}: 1.00")
+    expect(subject.perform).to include("#{inventory2.supplier.email}: 2.00")
+  end
+end
+
 describe QuoteRequest, ".search" do
   let(:location_name) { "Bo" }
   let(:product_name) { "findme" }
