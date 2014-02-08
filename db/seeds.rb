27ff8a4cd4e41fc367cb03f6c38e96f72b5235e3
Fergal Condron <condrof@gmail.com>
2014-02-08 12:05:07 +0000

    added seeds for suppliers, product, inventory

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Fabricator(:supplier) do
+  email { Faker::Internet.safe_email }
+  created_at { DateTime.now }
+  updated_at { DateTime.now }
+  password { "testtest" }
+end
+
+Fabricator(:product) do
+  name { Faker::Company.name }
+  created_at { DateTime.now }
+  updated_at { DateTime.now }
+end
+
+Fabricator(:inventory) do
+  product_id { rand(1..50) }
+  supplier_id { rand(1..10) }
+  price { rand(1..150)* rand() }
+  created_at { DateTime.now }
+  updated_at { DateTime.now }
+end
+
+10.times { Fabricate(:supplier) }
+50.times { Fabricate(:product) }
+150.times { Fabricate(:inventory) }
