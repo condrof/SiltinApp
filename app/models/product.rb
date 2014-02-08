311048a0c7fb6e52f8c8ddbf1e03586530bc9254
Fergal Condron <condrof@gmail.com>
2014-02-08 12:13:39 +0000

    model relation between product and supplier set up

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+	has_many :inventories
+	has_many :suppliers, through: :inventories, :foreign_key => :product_id	
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -3,4 +3,7 @@ class Supplier < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :inventories
+  has_many :products, through: :inventories, :foreign_key => :supplier_id	
 end
