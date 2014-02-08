1440c1c59d08a622a9fdff3f3f187c16f06a06ab
Chris McGrath <chris@octopod.info>
2014-02-08 14:10:41 +0000

    SMS request callbacks don't have authenticity token

--- app/controllers/sms_requests_controller.rb
+++ app/controllers/sms_requests_controller.rb
@@ -1,4 +1,6 @@
 class SmsRequestsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def create
     xml = Twilio::TwiML.build do |response|
       response.message "Sorry, we didn't understand your request."
