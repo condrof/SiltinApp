860f0b8f3a70448c0b4908d6447fee554b48aba1
Chris McGrath <chris@octopod.info>
2014-02-09 14:29:49 +0000

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140209134129) do
+ActiveRecord::Schema.define(version: 20140209134829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
