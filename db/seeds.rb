ed809fd981992b6f73234b59810f47822d6783a2
Chris McGrath <chris@octopod.info>
2014-02-09 12:17:49 +0000

    Fix seed locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,12 +39,11 @@ Fabricator(:unit) do
   created_at { DateTime.now }
   updated_at { DateTime.now }
 end
-
-@latlong = [ { lat: 7.964722, long: 11.738333  }, 
-             { lat: 8.633333, long: 12.05  }, 
-             { lat: 8.633333, long: 10.983333  }, 
-             { lat: 7.548889, long: 11.1875  }, 
-             { lat: 8.683333, long: 12.533333  } ]
+@latlong = [ { lat: 8.484146, long: -13.22867  }, # Freetown
+             { lat: 8.1605556, long: -12.4333333  }, # Moyamba
+             { lat: 7.288388999999999, long: -11.523228  }, # Bo
+             { lat: 8.42109, long: -12.53414  }, # Makeni
+             { lat: 9.5833333, long:  -11.55  } ] # Kabala
 2.times { @latlong.each {|l| Fabricate(:supplier, latitude: l[:lat], longitude: l[:long]) } }
 ["kg", "nr", "m2"].each {|new_name| Fabricate(:unit, name: new_name )}
 50.times { Fabricate(:product, unit_id: Unit.first.id) }
