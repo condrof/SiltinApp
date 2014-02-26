947b9e8aaabbb514731e4d9986aec764759f32fe
Chris McGrath <chris@octopod.info>
2014-02-26 22:17:40 +0000

    Add first simple scenario (steps not implemented)
    Already been useful in bringing up some questions I've put in as comments in
    the scenario for now

---
+++ features/step_definitions/sms_search_steps.rb
@@ -0,0 +1,11 @@
+Given(/^Suppliers for the (.*) product near (.*)$/) do |product_name, location_name|
+  pending
+end
+
+When(/^I SMS a quote for a (.*) near (.*)$/) do |product_name, location_name|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should get a list of suppliers near (.*) with thier price for a (.*)$/) do |location_name, product_name|
+  pending # express the regexp above with the code you wish you had
+end
